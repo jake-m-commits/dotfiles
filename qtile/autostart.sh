8b0b329735348eccea427844f10195ea2a2cc133
Jake Martin <jm9357481@gmail.com>
2022-02-09 19:56:20 -0800

    update qtile

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -14,5 +14,6 @@ extern=DP2
 if xrandr | grep "$extern disconnected"; then
   xrandr --output "$extern" --off --output "$intern" --auto
 else
-  xrandr --output "$extern" --primary --auto --output "$intern" --below "$extern" --auto
+  # xrandr --output "$extern" --primary --auto --output "$intern" --below "$extern" --auto
+  xrandr --output "$intern" --off --output "$extern" --auto
 fi
